34ec4cbb783e4e0681aeec0920b195e7531b637b
Mihnea Dobrescu-Balaur <mihnea@linux.com>
2013-11-09 18:17:12 +0200

    dommy

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-1978"]]
+                 [org.clojure/clojurescript "0.0-1978"]
+                 [prismatic/dommy "0.1.1"]]
   :plugins [[lein-cljsbuild "0.3.4"]]
 
   :cljsbuild {
