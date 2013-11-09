809123dfd6130a8dfb0139c1aab0e6c55e62e2e3
Andrei Oprea <andrei.br92@gmail.com>
2013-11-10 00:25:37 +0200

    Add marker to current location

--- src-cljs/where_you_at/main.cljs
+++ src-cljs/where_you_at/main.cljs
@@ -3,9 +3,14 @@
     (:require [dommy.core :as dom]))
 
 (def *connected* false)
+<<<<<<< HEAD
 (def *peer* nil)
 (def *connection* nil)
 
+(def *peer* (js/Peer. (js-obj "key" "0uudvj3cp4fe0zfr"
+                            "id" "test1")))
+(def *connection* (.connect *peer* "test2"))
+(def *mapInstance* nil)
 
 (defn log [stuff]
   (.log js/console stuff))
@@ -13,7 +18,11 @@
 (defn send-location-data [position]
     (let [coords (aget position "coords")
           latitude (aget coords "latitude")
-          longitude (aget coords "longitude")]
+          longitude (aget coords "longitude")
+          latLng (js/window.google.maps.LatLng. latitude longitude)]
+      (center-map latLng)
+      (place-marker latLng)
+      (log latLng)
       (when *connected*
         (log "sending")
         (.send *connection* (str latitude " " longitude)))))
@@ -26,11 +35,25 @@
         longitude (read-string (data 1))]
     (log (str "Received" latitude " " longitude))))
 
-
 (defn ^:export toggle-connected []
   (log "toggled")
+
+(defn place-marker [latLng]
+  (google.maps.Marker. (js-obj "position" latLng
+                              "map" *mapInstance*
+                              "title" "Where you at")))
+
+(defn process-recieved-data [data]
+  (let [words (clojure.string/split data #" ")]))
+
+
+(defn toggle-connected []
+>>>>>>> Add marker to current location
   (set! *connected* (not *connected*)))
 
+(defn center-map [latLng]
+  (log *mapInstance*)
+  (.setCenter *mapInstance* latLng))
 
 (defn map-config-obj []
   (js-obj "zoom" 12
@@ -40,7 +63,7 @@
 (defn ^:export init []
   (.watchPosition js/navigator.geolocation send-location-data)
   (window/google.maps.Map. (sel1 :#map) (map-config-obj)))
-
+  (set! *mapInstance* (window/google.maps.Map. (sel1 :#map) (map-config-obj))))
 
 (set! (.-onload js/window) init)
 
