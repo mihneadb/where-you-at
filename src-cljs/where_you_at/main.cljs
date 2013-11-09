b2eb738b09133e3633881cbc5af2ee7373d3638b
Mihnea Dobrescu-Balaur <mihnea@linux.com>
2013-11-09 19:20:53 +0200

    listen for the position

--- src-cljs/where_you_at/main.cljs
+++ src-cljs/where_you_at/main.cljs
@@ -1,4 +1,16 @@
-(ns where-you-at.main)
+(ns where-you-at.main
+    (:use-macros [dommy.macros :only [node sel1 sel by-id]])
+    (:require [dommy.core :as dom]))
 
-(.write js/document "Hello!")
+(defn log [stuff]
+  (.log js/console stuff))
+
+(defn process-location-data [position]
+    (let [coords (aget position "coords")
+          latitude (aget coords "latitude")
+          longitude (aget coords "longitude")]
+    (log (str latitude " " longitude))))
+
+(.watchPosition js/navigator.geolocation process-location-data)
+
 
