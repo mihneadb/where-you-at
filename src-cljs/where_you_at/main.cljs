894d5fb9ddb4e901429551a3a358f4e6ae385758
Mihnea Dobrescu-Balaur <mihnea@linux.com>
2013-11-09 20:04:33 +0200

    bootstrapped peerjs

--- src-cljs/where_you_at/main.cljs
+++ src-cljs/where_you_at/main.cljs
@@ -2,15 +2,32 @@
     (:use-macros [dommy.macros :only [node sel1 sel by-id]])
     (:require [dommy.core :as dom]))
 
+(def *connected* false)
+(def *peer* (js/Peer. (js-obj "key" "0uudvj3cp4fe0zfr"
+                            "id" "test1")))
+(def *connection* (.connect *peer* "test2"))
+
 (defn log [stuff]
   (.log js/console stuff))
 
-(defn process-location-data [position]
+(defn send-location-data [position]
     (let [coords (aget position "coords")
           latitude (aget coords "latitude")
           longitude (aget coords "longitude")]
-    (log (str latitude " " longitude))))
+      (when *connected*
+        (.send *connection* (str latitude " " longitude)))))
 
-(.watchPosition js/navigator.geolocation process-location-data)
+(defn process-recieved-data [data]
+  (let [words (clojure.string/split data #" ")]))
 
+(.watchPosition js/navigator.geolocation send-location-data)
+
+
+
+
+(defn toggle-connected []
+  (set! *connected* (not *connected*)))
+
+
+(.on *connection* "open" toggle-connected)
 
